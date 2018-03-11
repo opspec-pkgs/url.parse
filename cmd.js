46f7dc517a1419f98286cffa3d2588ac89598179
Chris Dostert <chris.dostert@live.com>
2018-03-11 00:19:24 -0800

    chore) use WHATWG url

--- cmd.js
+++ cmd.js
@@ -1,5 +1,25 @@
-const url = require('url');
+const { URL } = require('url');
 const fs = require('fs');
 
-const urlObject =  url.parse(fs.readFileSync('/url').toString());
-fs.writeFileSync(JSON.stringify(urlObject));
+const urlObject = new URL(fs.readFileSync('/url'));
+
+const searchParams = {};
+for (const [name, value] of urlObject.searchParams) {
+  searchParams[name] = value;
+}
+
+const url = {
+  origin: urlObject.origin,
+  protocol: urlObject.protocol,
+  username: urlObject.username,
+  password: urlObject.password,
+  host: urlObject.host,
+  hostname: urlObject.hostname,
+  port: urlObject.port,
+  pathname: urlObject.pathname,
+  search: urlObject.search,
+  searchParams,
+  hash: urlObject.hash,
+};
+
+fs.writeFileSync('/url', JSON.stringify(urlObject));
